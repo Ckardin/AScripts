5a889fe8bebb2e111b39e619b41b82d64380697d
F&nµx <fenyx.ckardin@gmail.com>
2024-02-18 15:13:52 +0100

    Fix error when call() Spaces lua code with interpreter.

--- bin/Spaces.lua
+++ bin/Spaces.lua
@@ -1,11 +1,11 @@
 s_Func = dofile("func.lua")
 
-if #arg < 2 then
+if #arg < 3 then
     print("You must specify number of spaces to print.")
 elseif #arg > 2 then
     print("You must specify exactly 1 parameter.")
 else
-    if type(arg[1]) != 'number' then
+    if type(arg[1]) ~= 'number' then
         print("Invalid parameter.")
     else
         local nspace = arg[1] .. ""
