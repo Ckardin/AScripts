5047b99b82f90868b2c06d438e9ca4e6bd55f533
F&nµx <fenyx.ckardin@gmail.com>
2024-03-10 17:37:24 +0100

    Add {Diff} to AScripts.

---
+++ src/Diff.cpp
@@ -0,0 +1,48 @@
+/*
+---------------------------------------------
+|    ____                                   |
+|   /\  _`\                                 |
+|   \ \ \L\_\ __    ___   __  __   __  _    |
+|    \ \  _\/'__`\/' _ `\/\ \/\ \ /\ \/'\   |
+|     \ \ \/\  __//\ \/\ \ \ \_\ \\/>  </   |
+|      \ \_\ \____\ \_\ \_\/`____ \/\_/\_\  |
+|       \/_/\/____/\/_/\/_/`/___/> \//\/_/  |
+|                             /\___/        |
+|                             \/__/         |
+|                                           |
+---------------------------------------------
+*/
+
+/// @file Diff.cpp
+/// @brief Source de AScripts/Diff
+/// @author F&nµx
+/// @version 1.0
+/// @date 10/03/2024
+
+#include "AScripts.h"
+
+int main(int argc, char *argv[])
+{
+    if(argc != 3)
+    {
+        std::cout << "You must specifiy exactly 2 directories for comparing themselves." <<std::endl;
+        return -1;
+    }
+
+    if(!std::filesystem::exists(argv[1]) || !std::filesystem::is_directory(argv[1]))
+    {
+        std::cout << "Path1 doesn't exist or isn't a directory." <<std::endl;
+        return -2;
+    }
+
+    if(!std::filesystem::exists(argv[2]) || !std::filesystem::is_directory(argv[2]))
+    {
+        std::cout << "Path2 doesn't exist or isn't a directory." <<std::endl;
+        return -3;
+    }
+
+    if(CompareDir(argv[1], argv[2])) std::cout << "Same directory" <<std::endl;
+    else                             std::cout << "Not same directory" <<std::endl;
+
+    return 0;
+}
